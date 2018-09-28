066f80b4ff652621ea1f0f807f88831a529ece02
Lao-Toshyo <laos@localhost.localdomain>
2018-09-28 11:33:31 +0700

    add data

---
+++ data/_urls.php
@@ -0,0 +1,56 @@
+<?php
+
+$words["URL_INTRO_IMAGE"] = [
+ "PRODUCT" => "",
+ "COLOR" => "",
+ "ALL" => "url(Page-Show,cid,W20)",
+ "" => "",
+];
+$words["URL_BLOC_1"] = [
+ "PRODUCT" => "",
+ "COLOR" => "",
+ "ALL" => "url(Page-Show,cid,W20)",
+ "" => "",
+];
+$words["URL_BLOC_2"] = [
+ "PRODUCT" => "",
+ "COLOR" => "",
+ "ALL" => "url(Page-Show,cid,Campagne)",
+ "" => "",
+];
+$words["URL_PRODUCT_1-URL"] = [
+ "PRODUCT" => "H18BASALT",
+ "COLOR" => "0002",
+ "ALL" => "url(Product-Show,pid,H18BASALT,dwvar_H18BASALT_color,0002)",
+ "" => "url(Product-Show,pid,H18BASALT,dwvar_H18BASALT_color,0002)",
+];
+$words["URL_PRODUCT_2-URL"] = [
+ "PRODUCT" => "H18RIPPER",
+ "COLOR" => "1066",
+ "ALL" => "url(Product-Show,pid,H18RIPPER,dwvar_H18RIPPER_color,1066)",
+ "" => "url(Product-Show,pid,H18RIPPER,dwvar_H18RIPPER_color,1066)",
+];
+$words["URL_PRODUCT_3-URL"] = [
+ "PRODUCT" => "H18FASTESTDG",
+ "COLOR" => "0186",
+ "ALL" => "url(Product-Show,pid,H18FASTESTDG,dwvar_H18FASTESTDG_color,0186)",
+ "" => "url(Product-Show,pid,H18FASTESTDG,dwvar_H18FASTESTDG_color,0186)",
+];
+$words["URL_PRODUCT_1-PRICE"] = [
+ "PRODUCT" => "H18BASALT",
+ "COLOR" => "3661601628160",
+ "ALL" => "include(Product-GetProductInfo, 'pid',3661601628160, 'prop', price)",
+ "" => "",
+];
+$words["URL_PRODUCT_2-PRICE"] = [
+ "PRODUCT" => "H18RIPPER",
+ "COLOR" => "3661601625442",
+ "ALL" => "include(Product-GetProductInfo, 'pid',3661601625442, 'prop', price)",
+ "" => "",
+];
+$words["URL_PRODUCT_3-PRICE"] = [
+ "PRODUCT" => "H18FASTESTDG",
+ "COLOR" => "3661601633416",
+ "ALL" => "include(Product-GetProductInfo, 'pid',3661601633416, 'prop', price)",
+ "" => "",
+];
---
+++ data/_words.php
@@ -0,0 +1,82 @@
+<?php
+
+$words["TXT_BLOC_1"] = [
+ "ALL" => "W20",
+ "FR" => "",
+ "UK" => "",
+ "IE" => "",
+ "ES" => "",
+ "DE" => "",
+ "IT" => "",
+ "FL" => "",
+];
+$words["TXT_TXT_1"] = [
+ "ALL" => "",
+ "FR" => "De nuit comme de jour, esprit nineties affirmé pour ces sneakers oversize, it-shoes de la rentrée. Coolness revisitée.",
+ "UK" => "Suitable for evening and day wear, these sneakers have a strong nineties feel and are the must-have shoes this autumn. Coolness revisited.",
+ "IE" => "",
+ "ES" => "Tanto de noche como de día, estas zapatillas grandes de espíritu noventero afirmado son los it-shoes de la nueva temporada. Frescura recreada.",
+ "DE" => "In der Nacht und am Tag, Oversize-Sneakers im Look der Nineties – Die IT-Shoes für den Herbst. Die neue Art der Coolness.",
+ "IT" => "Di notte come di giorno, acclamato spirito anni ‘90 per queste sneaker oversize, le it-shoes del rientro. Coolness rivisitata.",
+ "FL" => "",
+];
+$words["TXT_CTA_DISCOVER"] = [
+ "ALL" => "",
+ "FR" => "Découvrir",
+ "UK" => "Discover",
+ "IE" => "",
+ "ES" => "Descubrir",
+ "DE" => "Entdecken",
+ "IT" => "Scoprire",
+ "FL" => "",
+];
+$words["TXT_BLOC_2"] = [
+ "ALL" => "",
+ "FR" => "campagne",
+ "UK" => "campaign",
+ "IE" => "",
+ "ES" => "campaña",
+ "DE" => "kampagne",
+ "IT" => "campagna",
+ "FL" => "",
+];
+$words["TXT_CTA_DISCOVER"] = [
+ "ALL" => "",
+ "FR" => "Découvrir",
+ "UK" => "Discover",
+ "IE" => "",
+ "ES" => "Descubrir",
+ "DE" => "Entdecken",
+ "IT" => "Scoprire",
+ "FL" => "",
+];
+$words["TXT_PRODUCT_1"] = [
+ "ALL" => "",
+ "FR" => "Blouson en cuir",
+ "UK" => "Leather jacket",
+ "IE" => "",
+ "ES" => "Cazadora de cuero",
+ "DE" => "Motorrad-Lederjacke",
+ "IT" => "Giubbotto in pelle",
+ "FL" => "",
+];
+$words["TXT_PRODUCT_2"] = [
+ "ALL" => "",
+ "FR" => "Robe drapée à carreaux",
+ "UK" => "Draped plaid dress",
+ "IE" => "",
+ "ES" => "Vestido drapeado de cuadros",
+ "DE" => "Drapiertes Karo-Kleid",
+ "IT" => "Vestito drappeggiato a quadri",
+ "FL" => "",
+];
+$words["TXT_PRODUCT_3"] = [
+ "ALL" => "",
+ "FR" => "Sneakers à semelle oversize",
+ "UK" => "Mixed-material sneakers",
+ "IE" => "",
+ "ES" => "Sneakers en mezcla de materias",
+ "DE" => "Sneakers aus verschiedenen Materialien",
+ "IT" => "Sneakers multimateriale",
+ "FL" => "",
+];
